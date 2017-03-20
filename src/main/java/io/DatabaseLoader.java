f6d5e35006b902217efe79bfc29da45580017e8a
Joshua Petherick <peth@live.co.uk>
2017-03-20 17:05:17 +0000

    Changes made to DB script and database connection

--- src/main/java/io/DatabaseLoader.java
+++ src/main/java/io/DatabaseLoader.java
@@ -19,16 +19,6 @@ public class DatabaseLoader implements CommandLineRunner
     @Override
     public void run(String... strings) throws Exception
     {
-        // Only required to populate database on first run
-        /*
-        this.eRepository.save(new Employee("Frodo", "Baggins", "Ring bearer"));
-        this.eRepository.save(new Employee("Bilbo", "Baggins", "Burglar"));
-        this.eRepository.save(new Employee("Gandalf", "the Grey", "Wizard"));
-        this.eRepository.save(new Employee("Samwise", "Gamgee", "Gardener"));
-        this.eRepository.save(new Employee("Meriadoc", "Brandybuck", "Pony rider"));
-        this.eRepository.save(new Employee("Peregrin", "Took", "Pipe smoker"));
-        this.sRepository.save(new Shift("Dispose of One Ring", "ASAP", 2));
-        this.sRepository.save(new Shift("Help Aragorn become King", "Friday"));
-        */
+        // Insert when required
     }
 }
--- src/main/resources/db_script.sql
+++ src/main/resources/db_script.sql
@@ -1,20 +1,29 @@
 CREATE DATABASE IF NOT EXISTS `shifts_and_employees`;
 USE `shifts_and_employees`;
 
+-- Set checks to Zero, so can drop table
+SET FOREIGN_KEY_CHECKS=0;
 DROP TABLE IF EXISTS `employees`;
+
+-- Re-assign check so it checks key possible when creating table
+SET FOREIGN_KEY_CHECKS=1;
 CREATE TABLE `employees`(
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `firstName` varchar(255) NOT NULL,
   `lastName` varchar(255) NOT NULL,
   `description` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
-)ENGINE = InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
+)ENGINE = InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
-INSERT INTO 'employees' (firstName, lastName, description) VALUES ('Bill', 'Baggins', 'Chef');
-INSERT INTO 'employees' (firstName, lastName, description) VALUES ('Ted', 'Baker', 'Waiter');
-INSERT INTO 'employees' (firstName, lastName, description) VALUES ('Fred', 'Cumberlin', 'Supervisor');
+-- Insert new employees into table
+INSERT INTO `employees` (firstName, lastName, description) VALUES ('Bill', 'Baggins', 'Chef');
+INSERT INTO `employees` (firstName, lastName, description) VALUES ('Ted', 'Baker', 'Waiter');
+INSERT INTO `employees` (firstName, lastName, description) VALUES ('Fred', 'Cumberlin', 'Supervisor');
 
+SET FOREIGN_KEY_CHECKS=0;
 DROP TABLE IF EXISTS `shifts`;
+
+SET FOREIGN_KEY_CHECKS=1;
 CREATE TABLE `shifts`(
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `description` varchar(255)  DEFAULT NULL,
@@ -25,4 +34,6 @@ CREATE TABLE `shifts`(
   CONSTRAINT `fk_shift_employeeid` FOREIGN KEY(`employeeId`) REFERENCES `employees` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE = InnoDB DEFAULT CHARSET=utf8;
 
-INSERT INTO 'shifts' (description, day, employeeId) VALUES ('Prepare Medium-Rare Steak for Critic', 'Wednesday', 1);
+-- Insert new shifts into table
+INSERT INTO `shifts` (description, day, employeeId) VALUES ('Prepare Medium-Rare Steak for Critic', 'Wednesday', 1);
+INSERT INTO `shifts` (description, day, employeeId) VALUES ('Clean up the Kitchen', 'Thursday', NULL);
